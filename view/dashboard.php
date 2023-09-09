c9a3ccf5560d48042bdcdf4e9c9487a59ebd4b8c
Sultan <ahmad.sultan.2777@gmail.com>
2023-09-10 03:44:44 +0700

    Minor changes

--- view/dashboard.php
+++ view/dashboard.php
@@ -10,75 +10,76 @@
 
 <body>
   <main>
-    <h1>Kalender
+    <h1>Callendar
       <?= "$m/$y" ?>
     </h1>
     <section class="section-input">
       <a id="logout-btn" href="<?= APP_URL ?>logout/">Logout</a>
       <div>
         <form class="section-input-form" id="add-event">
-          <h2>Tambah Acara</h2>
+          <h2>New Event</h2>
           <div class="input">
-            <label for="nama">Nama acara</label>
-            <input type="text" name="nama" id="nama">
+            <label for="name">Event Name</label>
+            <input type="text" name="name" id="name">
           </div>
           <div class="input">
-            <label for="dari">Dari</label>
-            <input type="date" name="dari" id="dari" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
+            <label for="start">Starts From</label>
+            <input type="date" name="start" id="start" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
           </div>
           <div class="input">
-            <label for="sampai">Sampai</label>
-            <input type="date" name="sampai" id="sampai" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
+            <label for="end">Ends At</label>
+            <input type="date" name="end" id="end" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
           </div>
           <div class="input">
-            <label for="deskripsi">Deskripsi</label>
-            <textarea name="deskripsi" id="deskripsi"></textarea>
+            <label for="description">Description</label>
+            <textarea name="description" id="description"></textarea>
           </div>
-          <button id="input-submit" type="submit">Simpan</button>
+          <button id="input-submit" type="submit">Save</button>
         </form>
 
         <form class="section-input-form" id="edit-event">
-          <h2>Edit Acara</h2>
+          <h2>Edit Event</h2>
           <div class="input">
             <label for="eId">ID</label>
             <input type="text" name="eId" id="eId" placeholder="ID" readonly>
           </div>
           <div class="hidden" id="inputs">
             <div class="input">
-              <label for="nama-edit">Nama acara</label>
-              <input type="text" name="nama" id="nama-edit">
+              <label for="name-edit">Event Name</label>
+              <input type="text" name="name" id="name-edit">
             </div>
             <div class="input">
-              <label for="dari-edit">Dari</label>
-              <input type="date" name="dari" id="dari-edit" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
+              <label for="start-edit">Starts From</label>
+              <input type="date" name="start" id="start-edit" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
             </div>
             <div class="input">
-              <label for="sampai-edit">Sampai</label>
-              <input type="date" name="sampai" id="sampai-edit" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
+              <label for="end-edit">Ends At</label>
+              <input type="date" name="end" id="end-edit" value="<?= "$y-" . ($m < 10 ? "0$m" : $m) . "-01" ?>">
             </div>
             <div class="input">
-              <label for="deskripsi-edit">Deskripsi</label>
-              <textarea name="deskripsi" id="deskripsi-edit"></textarea>
+              <label for="description-edit">Description</label>
+              <textarea name="description" id="description-edit"></textarea>
             </div>
-            <button id="input-submit" type="submit">Simpan</button>
+            <button id="input-submit" type="submit">Save Changes</button>
           </div>
         </form>
       </div>
     </section>
+
     <section class="container">
       <div class="navigation">
         <a href="<?= APP_URL . "index.php?m=$prevMonth&y=$prevYear" ?>">&leftarrow;</a>
         <?php
         $uri = explode("/", $_SERVER['REQUEST_URI']);
         if (!preg_match("/^index\.php$/", end($uri))): ?>
-          <a href="<?= APP_URL ?>index.php">Kembali</a>
+          <a href="<?= APP_URL ?>index.php">Today</a>
         <?php endif ?>
         <a href="<?= APP_URL . "index.php?m=$nextMonth&y=$nextYear" ?>">&rightarrow;</a>
       </div>
       <div class="calendar">
         <?php foreach ($calendar as $day): ?>
-          <div class="day<?= $day['event'] > 0 ? ' ada-acara' : '' ?>" data-day="<?= $day['date'] ?>"
-            data-month="<?= $m ?>" data-year="<?= $y ?>">
+          <div class="day<?= $day['event'] > 0 ? ' filled' : '' ?>" data-day="<?= $day['date'] ?>" data-month="<?= $m ?>"
+            data-year="<?= $y ?>">
             <div class="day-date">
               <h2>
                 <?= $day['date'] ?>
@@ -87,7 +88,7 @@
             <ul class="event">
               <?php if (count($day['events']) > 0): ?>
                 <li>
-                  <b>Daftar acara</b>
+                  <b>Events</b>
                 </li>
                 <?php foreach ($day['events'] as $event): ?>
                   <li>
@@ -95,13 +96,13 @@
                       <?= $event['name'] ?>
                     </span>
                     <div class="buttons">
-                      <button class="delete-event-btn" data-e-id="<?= $event['e_id'] ?>">Hapus</button>
                       <button class="edit-event-btn" data-e-id="<?= $event['e_id'] ?>">Edit</button>
+                      <button class="delete-event-btn" data-e-id="<?= $event['e_id'] ?>">Remove</button>
                     </div>
                   </li>
                 <?php endforeach ?>
               <?php else: ?>
-                <li>Tidak ada acara</li>
+                <li>No Event</li>
               <?php endif ?>
             </ul>
           </div>
